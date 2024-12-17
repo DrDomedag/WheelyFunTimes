026efb762ed7a3e8636425900a2e25b4e35f3919
DrDomedag <joeli@kth.se>
2024-12-17 10:41:51 +0100

    Added basic test code for get_data_range

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import pykoda_main.src.pykoda as pk
 
 #print(pk.geoutils.flat_distance((0.1, 0.01), (0.2, 0.3)))
 
+company = "skane"
+date = "2020-08-21"
+
+start_hour = 15
+end_hour = 19
+
+df = pk.datautils.get_data_range(feed='TripUpdates', company=company, start_date=date,
+                                         start_hour=start_hour, end_hour=end_hour, merge_static=True)
+
+df.info()
+
+
